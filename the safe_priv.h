20c90345aa13fc30b0bb673b86263e5b331edb3d
AliAhmed94 <108825501+AliAhmed94@users.noreply.github.com>
2022-07-06 19:36:56 +0200

    Add files via upload

---
+++ the safe_priv.h
@@ -0,0 +1,21 @@
+/*
+ * the_safe.h
+ *
+ * Created: 3/15/2022 5:03:58 PM
+ *  Author: Ali
+ */ 
+
+
+#ifndef THE SAFE_H_
+#define THE SAFE_H_
+
+
+#define  EEPROM_STATUS_LOCATION    0x20
+#define  EEPROM_PASSWORD_LOCATION1 0x21
+#define  EEPROM_PASSWORD_LOCATION2 0x22
+#define  EEPROM_PASSWORD_LOCATION3 0x23
+#define  EEPROM_PASSWORD_LOCATION4 0x24
+#define MAX_TRIES 2
+
+
+#endif /* THE SAFE_H_ */
